c3386153a7b9a9d6d4bed6b9c03c0aab6f6baf37
Tsar <talip.saritas@gmail.com>
2025-02-09 16:55:50 +0000

    solved strncp

---
+++ c02/ex01/ft_strncpy.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n && src[i] !='0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return dest;	
+
+}
+int	main(void)
+{
+	char	source[] = "Hello World!";
+	char	destination[5];
+	ft_strncpy(destination, source, 5);
+	printf("%s\n", destination);
+}
