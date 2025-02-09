c7b569cd2e16c15ef1025fce253f21030c04ce0b
Tsar <talip.saritas@gmail.com>
2025-02-09 19:45:29 +0000

    changed rush04

--- rush00/rush04/rush04.c
+++ rush00/rush04/rush04.c
@@ -1,44 +1,45 @@
-void	ft_putchar(char c);
+#include <unistd.h>
 
-void	print_char(int col, int row, int x, int y)
+void    ft_putchar(char c)
 {
-	if ((row > 1 && row < x) && (col > 1 && row < y))
-	{
-		ft_putchar(' ');
-	}
-	else if ((row > 1 && row < x) && (col == 1 && row == y))
-	{
-		ft_putchar('-');
-	}
-	else if ((col > 1 && col < y) && (row == 1 || row ==x))
-	{
-		ft_putchar('|');
-	}
-	else
-	{
-		ft_putchar('o');
-	}
-
-void	rush(int x, int y)
-{
-	int	row;
-	int	col;
-
-	if ( x <= 0 || y <= 0)
-	{
-		return;
-	}
-	col = 1;
-	while (col <= y)
-	{
-		row = 1;
-		while (row <= x)
-		{
-			print_char(col, row, x, y);
-			row++;
-		}
-		ft_putchar('\n');
-		col++;
-	}
+        write(1, &c, 1);
 }
+
+void    print_char(int col, int row, int x, int y)
+{
+        if ((col == 1 && row == 1) || (col == x && row == y && x > 1 && y > 1))
+                ft_putchar('A');
+        else if ((col == x && row == 1) || (col == 1 && row == y))
+                ft_putchar('C');
+        else if (col == 1 || col == x || row == 1 || row == y)
+                ft_putchar('B');
+        else
+                ft_putchar(' ');
+}
+
+void    rush(int x, int y)
+{
+        int     row;
+        int     col;
+
+        if (x <= 0 || y <= 0)
+                return;
+        row = 1;
+        while (row <= y)
+        {
+                col = 1;
+                while (col <= x)
+                {
+                        print_char(col, row, x, y);
+                        col++;
+                }
+                ft_putchar('\n');
+                row++;
+        }
+}
+
+int     main(void)
+{
+        rush(5, 5);
+        return (0);
 }
