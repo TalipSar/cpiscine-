f9c1560d38d7686fe0e70c88c4de65fab9714e95
Tsar <talip.saritas@gmail.com>
2025-02-12 17:57:50 +0000

    cleaned branch

--- rush00/ex04/rush04.c
+++ rush00/ex04/rush04.c
@@ -3,64 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   rush04.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsaritas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: dsolbut <dsolbut@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/10 07:55:18 by tsaritas          #+#    #+#             */
-/*   Updated: 2025/02/10 08:16:59 by tsaritas         ###   ########.fr       */
+/*   Created: 2025/02/09 16:44:10 by dsolbut           #+#    #+#             */
+/*   Updated: 2025/02/09 16:51:15 by dsolbut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<unistd.h>
+#include <limits.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	print_char(int col, int row, int x, int y)
-{
-	if ((col == 1 && row == 1) || (col == x && row == y) && x > 1 && y > 1)
-	{
-		ft_putchar('A');
-	}
-	else if ((col == x && row == 1) || (col == 1 && row == y))
-	{
-		ft_putchar('C');
-	}
-	else if (col == 1 || col == x || row == 1 || row == x)
-	{
-		ft_putchar('B');
-	}
-	else
-	{
-		ft_putchar(' ');
-	}
-}
+void	ft_putchar(char c);
 
 void	rush(int x, int y)
 {
-	int	row;
-	int	col; 
+	int	r;
+	int	c;
 
-	if (x <= 0 || y <= 0)
+	if (x <= 0 || y <= 0 || x >= INT_MAX || y >= INT_MAX)
+		return ;
+	r = 1;
+	while (r <= y)
 	{
-		return;
-	}
-	row = 1; 
-	while (row <= y)
-	{
-		col = 1; 
-		while (col <= x)
+		c = 1;
+		while (c <= x)
 		{
-			print_char(col, row, x, y);
-			col++;
+			if ((c == 1 && r == 1) || (c == x && r == y && x > 1 && y > 1))
+				ft_putchar('A');
+			else if ((c == x && r == 1) || (c == 1 && r == y))
+				ft_putchar('C');
+			else if (c == 1 || c == x || r == 1 || r == y)
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			c++;
 		}
 		ft_putchar('\n');
-		row++;
+		r++;
 	}
-			
-}
-int	main(void)
-{
-	rush(1, 1);
 }
