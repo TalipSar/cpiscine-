f35cef00a3ebe1e177dc7d300d6769687fdea484
Tsar <talip.saritas@gmail.com>
2025-02-12 19:29:55 +0000

    cleanded strlen

--- c01/ex06/ft_strlen.c
+++ c01/ex06/ft_strlen.c
@@ -10,60 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include <stdio.h>
 
-void	ft_putchar(char c)
+int     ft_strlen(char *str)
 {
-	write(1, &c, 1);
-	return ;
-}
+        int     i;
 
-void	ft_putnbr(int nb)
-{
-	char	str[12];
-	int		i;
-	int		is_negative;
-
-	i = 0;
-	is_negative = 0;
-	if (nb == 0)
-		ft_putchar('0');
-	else if (nb < 0)
-	{
-		ft_putchar('-');
-		nb = -nb;
-	}
-	while (nb > 0)
-	{
-		str[i++] = nb % 10 + '0';
-		nb = nb / 10;
-	}
-	if (is_negative)
-		str[i++] = '-';
-	while (-- i >= 0)
-	{
-		ft_putchar(str[i]);
-	}
-}
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		i++;
-	}
-	ft_putnbr(i);
+        i = 0;
+        while(i < str[i])
+        {
+                i++;
+        }
+        return (i);
 }
 /*
-int	main(void)
+int     main(void)
 {
-	char	*str;
-
-	str = "salutation";
-	ft_strlen(str);
-	return (0);
+        char str[] = "Tugce";
+        int i;
+        i = ft_strlen(str);
+        printf("%d", i);
 }
 */
+
